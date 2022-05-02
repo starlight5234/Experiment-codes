8375f1f2dd455011b5e60f2ec57edb99b496ae75
starlight5234 <starlight5234@protonmail.ch>
2022-05-02 18:55:33 +0530

    Update Floyd warshall to take user input
    Change-Id: Ibb8e2fb81ac056d59542d67cafc7d2bddb7c92ac
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- floyd_warshall.c
+++ floyd_warshall.c
@@ -1,65 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define V 6
 #define INF 999999
 
-void printSolution(int dist[][V])
+void printSolution(int ad_mat[10][10], int n)
 {
-	for (int i = 0; i < V; i++){
-		for (int j = 0; j < V; j++){
-			if (dist[i][j] >= (INF%10))
-				printf("%7s", "INF");
-			else
-				printf ("%7d", dist[i][j]);
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			printf ("%d ", ad_mat[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-void floydWarshall (int graph[][V]){
-	int dist[V][V], i, j, k;
+void floydWarshall (int ad_mat[10][10], int n){
 
-	for (i = 0; i < V; i++)
-		for (j = 0; j < V; j++)
-			dist[i][j] = graph[i][j];
-
-	for (k = 0; k < V; k++){
-		for (i = 0; i < V; i++){
-			for (j = 0; j < V; j++){
-				if (dist[i][k] + dist[k][j] < dist[i][j])
-					dist[i][j] = dist[i][k] + dist[k][j];
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			for (int k = 0; k < n; k++){
+				if (ad_mat[k][j] > ad_mat[k][i] + ad_mat[i][j]){
+					ad_mat[k][j] = ad_mat[k][i] + ad_mat[i][j];
+				}
 			}
 		}
 	}
 
-	printSolution(dist);
+	printSolution(ad_mat, n);
 }
 
 int main(){
-    /* Testcase 1 */
-    int graph[V][V] = {
-           {0, INF, INF, INF, -1, INF},
-	   {1, 0, INF, 2, INF, INF},
-	   {INF, 2, 0, INF, INF, -8},
-           {-4, INF, INF, 0, 3, INF},
-           {INF, 7, INF, INF, 0, INF},
-           {INF, 5, 10, INF, INF, 0}
-	};
 
-    /* Testcase 2
-    int graph[V][V] = {
-           {0, 8, 5},
-	   {2, 0, INF},
-	   {INF, 1, 0},
-	};
-    */
-
-    printf("Given graph matrix is: \n");
-    printSolution(graph);
+	int ad_mat[10][10];
+	int n;
+	printf("Enter the number of vertices: ");
+	scanf("%d", &n);
+    printf("Etner cost in form of adjacency matrix: \n");
+    for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			scanf("%d", &ad_mat[i][j]);
+			if(i != j && ad_mat[i][j] == 0)
+				ad_mat[i][j] = INF;
+		}
+	}
 
     printf("\nShortest path matrix using Floyd Warshall's algorithm is: \n");
-    floydWarshall(graph);
+    floydWarshall(ad_mat, n);
 	return 0;
 }
-
