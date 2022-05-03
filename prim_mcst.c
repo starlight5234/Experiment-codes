cc12bff3b213b214fb9e9242dfecad1b6d1698d2
starlight5234 <starlight5234@protonmail.ch>
2022-05-04 00:17:29 +0530

    Useless refactor
    Change-Id: Icd88f9121995e0873f6eacef6373973f525f7a0f
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- prim_mcst.c
+++ prim_mcst.c
@@ -1,57 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define INF 999999
 
-void prim_mst(int cost[10][10], int n){
-    int visited[10]={0}, no_e=1, min_cost=0;
-    int min, a, b;
-
-    printf("\n");
+void prim(int ad_mat[10][10], int dim){
+    int visited[10] = {0};
+    int noOfEdgesVisited = 1, min_cost=0;
+    int min, a,b;
     visited[1]=1;
 
-    printf("MST Path:\n");
-
-    while(no_e<n){
-        min=INF;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(cost[i][j]<min){
-                    if(visited[i]!=0){
-                        min=cost[i][j];
-                        a=i;
-                        b=j;
+    while(noOfEdgesVisited < dim){
+        min = INF;
+        for(int i=1; i<=dim; i++){
+            for(int j=1; j<=dim; j++){
+                if(ad_mat[i][j]<min){
+                    if(visited[i] != 0){
+                        min = ad_mat[i][j];
+                        a = i;
+                        b = j;
                     }
                 }
             }
         }
 
+        printf("MST Path:\n");
         if(visited[b]==0){
             printf("%d to %d cost = %d\n",a,b,min);
-            min_cost=min_cost+min;
-            no_e++;
+            min_cost = min_cost + min;
+            noOfEdgesVisited++;
         }
 
-        visited[b]=1;
-        cost[a][b]=cost[b][a]=INF;
+        visited[b] = 1;
+        ad_mat[a][b] = ad_mat[b][a] = INF;
     }
 
     printf("\nMST Minimum cost is %d\n",min_cost);
 }
 
 int main(){
-    int cost[10][10],n;
-    printf("Enter number of nodes: ");
-    scanf("%d",&n);
-    printf("Enter cost in form of adjacency matrix: \n");
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            scanf("%d",&cost[i][j]);
-            if(cost[i][j]==0)
-              cost[i][j]=INF;
+    int ad_mat[10][10];
+    int dim;
+
+    printf("Enter the number of vertices: ");
+    scanf("%d", &dim);
+
+    printf("Enter cost in the form of adjacency matrix: " );
+    for(int i=1; i<=dim; i++){
+        for(int j=1; j<=dim; j++){
+            scanf("%d", &ad_mat[i][j]);
+            if(ad_mat[i][j] == 0){
+                ad_mat[i][j] = INF;
+            }
         }
     }
 
-    prim_mst(cost, n);
+    prim(ad_mat, dim);
     return 0;
 }
+
